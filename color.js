b1e9a1a03db5c5f98b18a44cbc9a688b18cecea2
Aaron Minnick <abminnick@gmail.com>
2022-05-31 20:33:13 -0700

    add TODO for leading 0 if hex value < 16

--- color.js
+++ color.js
@@ -1,5 +1,6 @@
 export default class Color {
   //TODO: generation should not allow for white, black, or colors which are too grey
+  //TODO: hex pairs need to prepend a 0 if value < 16"
   constructor() {
     this.red = Math.floor(Math.random()*255);
     this.redhex = this.red.toString(16);
