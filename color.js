d7ca91e4b4605765d2972d9a66331791c384441f
Aaron Minnick <abminnick@gmail.com>
2022-05-31 19:41:35 -0700

    add methods to shuffle hex characters or pairs (individual colors)

--- color.js
+++ color.js
@@ -8,4 +8,28 @@ export default class Color {
     this.bluehex = this.blue.toString(16);
     this.hex = this.redhex + this.greenhex + this.bluehex;
   }
+
+  shuffledHexCharArray() {
+    let shuffleArray = this.hex.split('');
+    /* shuffle array in-place using Durstenfeld shuffle algorithm */
+    for (let i = shuffleArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = shuffleArray[i];
+      shuffleArray[i] = shuffleArray[j];
+      shuffleArray[j] = temp;
+    }
+    return shuffleArray;
+  }
+
+  shuffledHexPairArray() {
+    let shuffleArray = [this.redhex, this.greenhex, this.bluehex];
+    /* shuffle array in-place using Durstenfeld shuffle algorithm */
+    for (let i = shuffleArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = shuffleArray[i];
+      shuffleArray[i] = shuffleArray[j];
+      shuffleArray[j] = temp;
+    }
+    return shuffleArray;
+  }
 }
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import Color from './color.js';
 let test = new Color();
 
 console.log(test);
+console.log(test.shuffledHexCharArray());
+console.log(test.shuffledHexPairArray());
